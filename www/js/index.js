4b6b970eb587bf8ab900b069a0edb188dc715ea8
marcialvarela <marcial.varela@gmail.com>
2015-01-29 09:34:26 +0100

    20150129_02

--- www/js/index.js
+++ www/js/index.js
@@ -7,7 +7,7 @@ var path = '';
 //var capture = navigator.device.capture;
 
 window.addEventListener('load', function () {
-    document.addEventListener("deviceReady", deviceReady, false);
+    document.addEventListener("deviceReady", onDeviceReady, false);
 }, false);
 
 function deviceReady() {
@@ -24,7 +24,7 @@ function deviceReady() {
     }
 }
 
-/*************************** CAPTURE AUDIO ***************************/
+/*************************** CAPTURE AUDIO - INI ***************************/
 // capture callback
 var captureSuccess = function(mediaFiles) {
     path = mediaFiles[0].fullPath;
@@ -59,6 +59,59 @@ function stopAudio() {
     alert('Entra en stopAudio');
 
 }
+/*************************** CAPTURE AUDIO - END ***************************/
 
-/*************************** CAPTURE AUDIO ***************************/
 
+
+
+
+/*************************** RECORD AUDIO - INI ***************************/
+function recordAudio() {
+    alert('entra recordAudio');
+    var src = "myrecording_001.amr";
+    var mediaRec = new Media(src, onSuccess, onError);
+
+    // Record audio
+    mediaRec.startRecord();
+    alert('startRecord');
+
+    // Stop recording after 10 sec
+    var recTime = 0;
+    var recInterval = setInterval(function() {
+        recTime = recTime + 1;
+        setAudioPosition(recTime + " sec");
+        if (recTime >= 10) {
+            clearInterval(recInterval);
+            mediaRec.stopRecord();
+        }
+    }, 1000);
+    alert('sale recordAudio');
+}
+// device APIs are available
+//
+function onDeviceReady() {
+    alert('entra onDeviceReady');
+    recordAudio();
+    alert('sale onDeviceReady');
+}
+
+// onSuccess Callback
+//
+function onSuccess() {
+    console.log("recordAudio():Audio Success");
+}
+
+// onError Callback
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+    'message: ' + error.message + '\n');
+}
+
+// Set audio position
+//
+function setAudioPosition(position) {
+    document.getElementById('audio_position').innerHTML = position;
+}
+
+/*************************** RECORD AUDIO - END ***************************/
